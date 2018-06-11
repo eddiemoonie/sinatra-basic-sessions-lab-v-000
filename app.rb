36b4cd0e29a25fd02ac06594dac8e5f86acc1323
Eddie Moon <eddiemoonie@gmail.com>
2018-06-11 21:40:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   get '/' do
     @session = session
 
     erb :index
   end
 
+  post '/checkout' do
 
+  end
 end
